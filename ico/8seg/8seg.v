6b87845c9fadc54833f372c309c9f34cdd48916a
Atanas Bachvaroff <bachvaroff@gmail.com>
2018-07-06 02:19:43 +0300

    8seg driver

--- ico/8seg/8seg.v
+++ ico/8seg/8seg.v
@@ -1,17 +1,17 @@
-module LFSR (i_Clk, i_Enable, i_Seed_DV, i_Seed_Data, o_LFSR_Data, o_LFSR_Done);
-	input i_Clk;
-	input i_Enable;
+module LFSR (clk, enable, i_Seed_DV, i_Seed_Data, o_LFSR_Data, o_LFSR_Done);
+	input clk;
+	input enable;
 	input i_Seed_DV;
 	input [NUM_BITS-1:0] i_Seed_Data;
 	output [NUM_BITS-1:0] o_LFSR_Data;
 	output o_LFSR_Done;
-	parameter NUM_BITS = 32;
+	parameter NUM_BITS = 16;
 	
 	reg [NUM_BITS:1] r_LFSR;
 	reg r_XNOR;
 	
-	always @(posedge i_Clk) begin
-		if (i_Enable == 1'b1) begin
+	always @(posedge clk) begin
+		if (enable == 1'b1) begin
 			if (i_Seed_DV == 1'b1) r_LFSR <= i_Seed_Data;
 			else r_LFSR <= { r_LFSR[NUM_BITS-1:1], r_XNOR };
 		end
@@ -210,7 +210,7 @@ module sixteen_bit_drv (clock, data, oe, drains, leds);
 				tetrade <= data[15:12];
 			end
 		endcase
-				
+		
 		if (oe == 1) begin
 			tetrade_sel <= tetrade_sel + 1;
 			drains <= tdrains;
@@ -223,23 +223,23 @@ module sixteen_bit_drv (clock, data, oe, drains, leds);
 	end
 endmodule
 
-module LFSR_TB (i_CLK, o_LED, drains, leds);
+module LFSR_ice40 (i_CLK, o_LED, drains, leds);
 	input i_CLK;
 	output [2:0] o_LED;
 	output [3:0] drains;
 	output [7:0] leds;
-	parameter c_NUM_BITS = 32;
+	parameter c_NUM_BITS = 16;
 	
 	reg [31:0] counter;
 	
-	reg r_Clk;
+	reg lfclk;
 	reg update;
 	wire [c_NUM_BITS-1:0] w_LFSR_Data;
 	wire w_LFSR_Done;
 	
 	LFSR #(c_NUM_BITS) LFSR_inst(
-		.i_Clk(r_Clk),
-		.i_Enable(1'b1),
+		.clk(lfclk),
+		.enable(1'b1),
 		.i_Seed_DV(1'b0),
 		.i_Seed_Data({c_NUM_BITS{1'b0}}),
 		.o_LFSR_Data(w_LFSR_Data),
@@ -265,8 +265,8 @@ module LFSR_TB (i_CLK, o_LED, drains, leds);
 		update <= ~update;
 	end
 	
-	always @(posedge counter[25]) begin
-		r_Clk <= ~r_Clk;
+	always @(posedge counter[22]) begin
+		lfclk <= ~lfclk;
 	end
 endmodule
 
