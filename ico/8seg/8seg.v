9c028a6ce62969789b31777e7dde58f21ce1f568
Atanas Bachvaroff <bachvaroff@gmail.com>
2018-07-15 12:17:47 +0300

    8seg driver

--- ico/8seg/8seg.v
+++ ico/8seg/8seg.v
@@ -7,13 +7,11 @@ module LFSR_8seg (i_CLK, o_LED, drains, leds);
 	
 	reg [31:0] counter;
 	
-	reg lfclk;
-	reg update;
 	wire [c_NUM_BITS-1:0] w_LFSR_Data;
 	wire w_LFSR_Done;
 	
 	LFSR #(c_NUM_BITS) LFSR_inst(
-		.clk(lfclk),
+		.clk(counter[24]),
 		.enable(1'b1),
 		.i_Seed_DV(1'b0),
 		.i_Seed_Data({c_NUM_BITS{1'b0}}),
@@ -23,7 +21,7 @@ module LFSR_8seg (i_CLK, o_LED, drains, leds);
 	
 	
 	sixteen_bit_drv drv(
-		.clock(update),
+		.clock(counter[17]),
 		.data(w_LFSR_Data[19:0]),
 		.oe(1'b1),
 		.drains(drains),
@@ -34,13 +32,5 @@ module LFSR_8seg (i_CLK, o_LED, drains, leds);
 	
 	always @(posedge i_CLK) begin
 		counter <= counter + 1;
-	end
-	
-	always @(posedge counter[17]) begin
-		update <= ~update;
-	end
-	
-	always @(posedge counter[24]) begin
-		lfclk <= ~lfclk;
 	end
 endmodule
