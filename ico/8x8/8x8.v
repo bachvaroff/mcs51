8da299724b755331ace0b4b329b62d2726343c46
Atanas Bachvaroff <bachvaroff@gmail.com>
2018-08-22 07:16:10 +0300

    8x8 driver

--- ico/8x8/8x8.v
+++ ico/8x8/8x8.v
@@ -1,36 +1,38 @@
-module LFSR_8x8 (i_CLK, o_LED, rows, columns);
-input i_CLK;
-output [2:0] o_LED;
+module LFSR_8x8 (CLK, LED, rows, columns);
+input CLK;
+output LED;
 output [7:0] rows;
 output [7:0] columns;
 
-parameter c_NUM_BITS = 64;
+localparam NUM_BITS = 64;
+localparam LFSR_CLK = 21;
+localparam UPDATE_CLK = 16;
 
 reg [31:0] counter;
 
-wire [c_NUM_BITS-1:0] w_LFSR_Data;
-wire w_LFSR_Done;
+wire [NUM_BITS-1:0] LFSR_REG;
+wire LFSR_DONE;
 
 LFSR #(c_NUM_BITS) LFSR_inst(
-	.clk(counter[21]),
+	.clk(counter[LFSR_CLK]),
 	.enable(1'b1),
 	.i_Seed_DV(1'b0),
-	.i_Seed_Data({c_NUM_BITS{1'b0}}),
-	.o_LFSR_Data(w_LFSR_Data),
-	.o_LFSR_Done(w_LFSR_Done)
+	.i_Seed_Data({ NUM_BITS{1'b0} }),
+	.o_LFSR_Data(LFSR_REG),
+	.o_LFSR_Done(LFSR_DONE)
 );
 
 sixtyfour_bit_drv drv(
-	.clock(counter[16]),
-	.data(w_LFSR_Data[63:0]),
+	.clock(counter[UPDATE_CLK]),
+	.data(LFSR_REG[63:0]),
 	.oe(1'b1),
 	.row(rows),
 	.column(columns)
 );
 
-assign o_LED = w_LFSR_Data[2:0];
+assign LED = counter[LFSR_CLK];
 
-always @(posedge i_CLK) begin
+always @(posedge CLK) begin
 	counter <= counter + 1;
 end
 
