8b7c2442b655a85e3bf0d425e0a032fb3c823405
Atanas Bachvaroff <bachvaroff@gmail.com>
2018-08-31 19:27:42 +0300

    64bit

--- ico/8x8/64bit_drv.v
+++ ico/8x8/64bit_drv.v
@@ -10,51 +10,51 @@ reg [7:0] trow;
 reg [7:0] tcolumn;
 	
 always @(posedge clock) begin
-	case (byte_sel)
-		3'b000: begin
-			trow <= 8'b00000001;
-			tcolumn <= data[7:0];
-		end
-		3'b001: begin
-			trow <= 8'b00000010;
-			tcolumn <= data[15:8];
-		end
-		3'b010: begin
-			trow <= 8'b00000100;
-			tcolumn <= data[23:16];
-		end
-		3'b011: begin
-			trow <= 8'b00001000;
-			tcolumn <= data[31:24];
-		end
-		3'b100: begin
-			trow <= 8'b00010000;
-			tcolumn <= data[39:32];
-		end
-		3'b101: begin
-			trow <= 8'b00100000;
-			tcolumn <= data[47:40];
-		end
-		3'b110: begin
-			trow <= 8'b01000000;
-			tcolumn <= data[55:48];
-		end
-		3'b111: begin
-			trow <= 8'b10000000;
-			tcolumn <= data[63:56];
-		end
-	endcase
-	
-	if (oe == 1) begin
-		byte_sel <= byte_sel + 1;
-		row <= trow;
-		column <= tcolumn;
-	end else begin
+	if (!oe) begin
+		trow <= 8'b00000000;
+		tcolumn <= 8'b00000000;
 		byte_sel <= 3'b000;
-		row <= 8'b00000000;
-		column <= 8'b00000000;
+	end else begin
+		case (byte_sel)
+			3'b000: begin
+				trow <= 8'b00000001;
+				tcolumn <= data[7:0];
+			end
+			3'b001: begin
+				trow <= 8'b00000010;
+				tcolumn <= data[15:8];
+			end
+			3'b010: begin
+				trow <= 8'b00000100;
+				tcolumn <= data[23:16];
+			end
+			3'b011: begin
+				trow <= 8'b00001000;
+				tcolumn <= data[31:24];
+			end
+			3'b100: begin
+				trow <= 8'b00010000;
+				tcolumn <= data[39:32];
+			end
+			3'b101: begin
+				trow <= 8'b00100000;
+				tcolumn <= data[47:40];
+			end
+			3'b110: begin
+				trow <= 8'b01000000;
+				tcolumn <= data[55:48];
+			end
+			3'b111: begin
+				trow <= 8'b10000000;
+				tcolumn <= data[63:56];
+			end
+		endcase
+		byte_sel <= byte_sel + 1;
 	end
 end
+
+assign row = trow;
+assign column = tcolumn;
 
 endmodule
 
