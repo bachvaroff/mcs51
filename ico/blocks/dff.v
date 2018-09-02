0ead33f90723d8b1aa5f22dba4b92de8ce979bf5
Atanas Bachvaroff <bachvaroff@gmail.com>
2018-09-03 01:59:03 +0300

    dff2

--- ico/blocks/dff.v
+++ ico/blocks/dff.v
@@ -8,3 +8,16 @@ always @(posedge CLK) begin
 end
 
 endmodule
+
+module dff(CLK, D, Q, Qdash);
+input CLK;
+input D;
+output reg Q;
+output reg Qdash;
+
+always @(posedge CLK) begin
+	Q <= D;
+	Qdash <= ~D;
+end
+
+endmodule
