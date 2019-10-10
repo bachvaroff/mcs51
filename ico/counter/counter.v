90df9055654d37786443d6bf11065f4abc30361a
Atanas Bachvaroff <bachvaroff@gmail.com>
2019-10-10 18:37:06 +0300

    144

--- ico/counter/counter.v
+++ ico/counter/counter.v
@@ -1,5 +1,6 @@
-module counter(CLK, BTTN, LEDS, LEDS_Gray, ACT_LED);
+module counter(CLK, CLKSEL, BTTN, LEDS, LEDS_Gray, ACT_LED);
 input wire CLK;
+input wire CLKSEL;
 input wire [3:0] BTTN;
 output wire [9:0] LEDS;
 output wire [9:0] LEDS_Gray;
@@ -9,11 +10,12 @@ reg [31:0] clk_div;
 reg [9:0] led_cnt;
 reg [2:0] act_led;
 
+wire sel, sel_up, sel_down;
 wire [3:0] pb;
 wire [3:0] pb_up;
 wire [3:0] pb_down;
 wire main_clk, reg_clk, db_clk;
-wire res[1:0];
+wire res[2:0];
 
 // binary
 assign LEDS = led_cnt;
@@ -32,14 +34,23 @@ assign ACT_LED = {
 assign reg_clk = clk_div[22];
 assign db_clk = clk_div[4];
 
-assign res[0] = pb[2];
-assign res[1] = pb[3];
+assign res[0] = pb[1];
+assign res[1] = pb[2];
+assign res[2] = pb[3];
 
 initial begin
 	clk_div = 0;
 	led_cnt = 0;
 	act_led = 0;
 end
+
+debounce BTTNCLKSEL(
+	.CLK(db_clk),
+	.PB(CLKSEL),
+	.PB_state(sel),
+	.PB_down(sel_down),
+	.PB_up(sel_up)
+);
 
 debounce BTTN0(
 	.CLK(db_clk),
@@ -75,23 +86,27 @@ debounce BTTN3(
 
 clksel selector(
 	.CLK({pb[0], reg_clk}),
-	.ALTSEL(pb[1]),
+	.ALTSEL(sel),
 	.OUTCLK(main_clk)
 );
 
 always @(posedge CLK) begin
-	clk_div <= clk_div + 1;
+	clk_div <= clk_div + 32'd1;
 end
 
 always @(posedge reg_clk) begin
-	act_led <= act_led + 1;
+	act_led <= act_led + 3'd1;
 end
 
-always @(posedge main_clk or posedge res[0] or posedge res[1]) begin
-	if (res[0] || res[1]) begin
-		led_cnt <= 0;
+always @(posedge main_clk or posedge res[0] or posedge res[1] or posedge res[2]) begin
+	if (res[0]) begin
+		led_cnt <= 10'b0000000000;
+	end else if (res[1]) begin
+		led_cnt <= 10'b0101010101;	
+	end else if (res[2]) begin
+		led_cnt <= 10'b1010101010;
 	end else begin
-		led_cnt <= led_cnt + 1;
+		led_cnt <= led_cnt + 10'b0000000001;
 	end
 end
 
