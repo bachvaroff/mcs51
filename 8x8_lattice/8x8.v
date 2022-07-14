d272acfded504ff96ca0f88069261e62a81edbec
Atanas Bachvaroff <bachvaroff@gmail.com>
2022-07-14 06:48:22 +0300

    14072022

--- 8x8_lattice/8x8.v
+++ 8x8_lattice/8x8.v
@@ -1,23 +1,23 @@
 module LFSR_8x8 (CLK, LED_CLK, LED_DONE, rows, columns);
-input CLK;
-output LED_CLK;
-output LED_DONE;
-output [7:0] rows;
-output [7:0] columns;
+input wire CLK;
+output wire LED_CLK;
+output wire LED_DONE;
+output wire [7:0] rows;
+output wire [7:0] columns;
 
 localparam NUM_BITS = 64;
 localparam LFSR_CLK = 21;
 localparam UPDATE_CLK = 16;
 
 reg [31:0] counter;
-reg [NUM_BITS-1:0] LFSR;
+wire [NUM_BITS-1:0] LFSR;
 
 LFSR #(NUM_BITS) LFSR_inst(
 	.CLK(counter[LFSR_CLK]),
 	.E(1'b1),
 	.RESET(1'b0),
-	.SEED({ NUM_BITS{1'b0} }),
-	.LFSR(LFSR),
+	.SEED_VAL({ NUM_BITS{1'b0} }),
+	.LFSR_VAL(LFSR),
 );
 
 uint64_drv drv(
