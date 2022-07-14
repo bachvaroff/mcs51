53b2f4d57d0e347b72326c8f355d8e465b56b97a
Atanas Bachvaroff <bachvaroff@gmail.com>
2022-07-14 06:39:39 +0300

    14072022

--- 8seg/8seg.v
+++ 8seg/8seg.v
@@ -1,31 +1,31 @@
 module LFSR_8seg (CLK, LED_CLK, LED_DONE, drains, leds);
-input CLK;
-output LED_CLK;
-output LED_DONE;
-output [7:0] drains;
-output [7:0] leds;
+input wire CLK;
+output wire LED_CLK;
+output wire LED_DONE;
+output wire [7:0] drains;
+output wire [7:0] leds;
 
 localparam NUM_BITS = 32;
 localparam LFSR_CLK = 25;
 localparam UPDATE_CLK = 16;
 
 reg [31:0] counter;
-reg [NUM_BITS-1:0] LFSR;
+wire [NUM_BITS-1:0] LFSR;
 
 LFSR #(NUM_BITS) LFSR_inst(
 	.CLK(counter[LFSR_CLK]),
 	.E(1'b1),
 	.RESET(1'b0),
-	.SEED({ NUM_BITS{1'b0} }),
-	.LFSR(LFSR),
+	.SEED_VAL({ NUM_BITS{1'b0} }),
+	.LFSR_VAL(LFSR),
 );
 
 uint32_drv drv(
 	.CLK(counter[UPDATE_CLK]),
 	.OE(1'b1),
 	.data(LFSR),
-	.drains(drains),
-	.leds(leds)
+	.LS_NFET(drains),
+	.HS_NFET_PFET(leds)
 );
 
 always @(posedge CLK) begin
