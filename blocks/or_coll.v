b946240eac1f35a8fe22cb4048165baadb9db3c3
Atanas Bachvaroff <bachvaroff@gmail.com>
2022-10-01 10:12:03 +0300

    01102022

---
+++ blocks/or_coll.v
@@ -0,0 +1,9 @@
+module gray_encoder(register, selector, out);
+parameter LEN = 8;
+input wire [(LEN - 1):0] register;
+input wire [(LEN - 1):0] selector;
+output wire out;
+
+assign out = |(register & selector);
+
+endmodule
