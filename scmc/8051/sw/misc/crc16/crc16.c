8f2bad955b45b19fd10e0fea862f0687db2f32f2
Atanas Bachvaroff <bachvaroff@gmail.com>
2023-01-08 11:16:11 +0200

    08012023

--- scmc/8051/sw/misc/crc16/crc16.c
+++ scmc/8051/sw/misc/crc16/crc16.c
@@ -1,17 +1,39 @@
 #include <mcs51/at89x52.h>
-#include "paulmon2.h"
+#include <stdio.h>
 
 #define ACC_INITIAL 0xffffu
 #define POLY 0x1021u
 #define ACC_FINAL 0x0000u
 
-static void ccrcb_init(unsigned short *r) {
+#define SADDR 0x0000u
+#define FADDR 0x2000u
+
+#define pm2_entry_cout          0x0030
+#define pm2_entry_cin           0x0032
+
+int putchar(int c) __naked {
+        (void)c;
+        __asm
+        mov a, dpl
+        ljmp pm2_entry_cout
+        __endasm;
+}
+
+int getchar(void) {
+        __asm
+        lcall   pm2_entry_cin
+	clr dph
+        mov dpl, a
+        __endasm;
+}
+
+static void ccrcb_init(unsigned int *r) {
 	*r = ACC_INITIAL;
 	return;
 }
 
-static void ccrcb(unsigned short *r, unsigned char oct) {
-	unsigned short rt;
+static void ccrcb(unsigned int *r, unsigned char oct) {
+	unsigned int rt;
 	unsigned char bitp;
 	
 	for (rt = *r, bitp = 0x80u; bitp; bitp >>= 1)
@@ -19,23 +41,24 @@ static void ccrcb(unsigned short *r, unsigned char oct) {
 	*r = rt;
 }
 
-static void ccrcb_finish(unsigned short *r) {
+static void ccrcb_finish(unsigned int *r) {
 	*r ^= ACC_FINAL;
 }
 
 void main(void) {
 	unsigned char *monitor;
-	unsigned short crc;
+	unsigned int crc;
 	
 	ccrcb_init(&crc);
-	for (monitor = (unsigned char *)0x0000u; (unsigned short)monitor < 0x2000u; monitor++)
+	for (monitor = (unsigned char *)SADDR; (unsigned int)monitor < FADDR; monitor++)
 		ccrcb(&crc, *monitor);
 	ccrcb_finish(&crc);
 	
-	pm2_phex16(crc);
-	pm2_newline();
-	(void)pm2_cin();
+	printf("CRC16(0x%04x, 0x%04x) = 0x%04x\n\r", SADDR, FADDR, crc);
+	(void)getchar();
 	
-	pm2_restart();
+	__asm
+		ljmp 0
+	__endasm;
 }
 
