b1eca422e97a1d94992404f7d4074ec48a8f5baf
Atanas Bachvaroff <bachvaroff@gmail.com>
2023-11-10 18:17:12 +0200

    20231110

---
+++ scmc/8051/sw/misc/disp/initial.h
@@ -0,0 +1,11 @@
+#ifndef _INITIAL_
+#define _INITIAL_
+
+/*
+#define INITIAL_MSG	"Sieg Heil! Sieg Heil! Viktoria! Sieg Heil! Viktoria! "
+*/
+
+#define INITIAL_MSG	"Jedem das Seine! IVSTITIA SVVM CVIQVE DISTRIBVIT "
+
+#endif
+
