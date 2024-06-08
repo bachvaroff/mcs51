d69d9d1e3fcfd37ed8f16ecadb2cdebc32665bd3
Atanas Bachvaroff <bachvaroff@gmail.com>
2024-06-08 12:37:16 +0300

    2024060800

--- skip/skip.v
+++ skip/skip.v
@@ -5,16 +5,18 @@ output wire LED_SCLK;
 output wire LED_B0;
 
 localparam LEN = 16;
-localparam UPDATE_CLK = 23;
+localparam UPDATE_BIT = 24;
+localparam EN_BIT = 30;
 
 reg [31:0] counter = 32'b0;
 
-assign LED_CLK = counter[UPDATE_CLK];
+assign LED_CLK = counter[UPDATE_BIT];
+assign EN = counter[EN_BIT];
 
 skipring #(LEN) skip_clock(
 	.iCLK(LED_CLK),
 	.RST(1'b0),
-	.E(1'b1),
+	.E(EN),
 	.MASK(LEN'b0011010001000101),
 	.oCLK(LED_SCLK),
 	.oB0(LED_B0)
