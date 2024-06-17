97fd84ba38ee3cceb08d529bdacb43287ea4855d
Atanas Bachvaroff <bachvaroff@gmail.com>
2024-06-17 07:37:05 +0300

    2024061700

--- tmp/toggle_lattice/toggle.v
+++ tmp/toggle_lattice/toggle.v
@@ -3,12 +3,14 @@ input wire iCLK, iD;
 output wire oD;
 
 reg [1:0] Dreg = 2'b00;
+reg Oreg = 1'b0;
 
-assign oD = ^Dreg;
+assign oD = Oreg;
 
 always @(posedge iCLK) begin
 	Dreg[0] <= iD;
 	Dreg[1] <= Dreg[0];
+	Oreg <= ^Dreg;
 end
 
 endmodule
