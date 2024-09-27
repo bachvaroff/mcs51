08363f768a7228dcd2c8607228547e851d4aca7d
Atanas Bachvaroff <bachvaroff@gmail.com>
2024-09-27 19:14:05 +0300

    2024092700 / reorg'

--- blocks/or_coll.v
+++
@@ -1,9 +0,0 @@
-module or_coll(register, selector, out);
-parameter LEN = 8;
-input wire [(LEN - 1):0] register;
-input wire [(LEN - 1):0] selector;
-output wire out;
-
-assign out = |(register & selector);
-
-endmodule
